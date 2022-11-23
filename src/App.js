9cef750408ecf3a9237d1f4a6437f0f1884e8e0d
sohagislampk <sohagislampk@gmail.com>
2022-11-24 02:36:48 +0600

    React router set, layout and folder structure created

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { RouterProvider } from 'react-router-dom';
+import routes from './Routes/Routes/Routes';
 
 function App() {
   return (
-    <div className="App">
-      <button className="btn">Button</button>
-      <button className="btn btn-primary">Button</button>
-      <button className="btn btn-secondary">Button</button>
-      <button className="btn btn-accent">Button</button>
-      <button className="btn btn-ghost">Button</button>
-      <button className="btn btn-link">Button</button>
-    </div>
+    <RouterProvider router={routes}></RouterProvider>
   );
 }
 
