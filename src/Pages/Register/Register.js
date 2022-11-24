c7141971d7ddd8169ac39d63a143a741f0915c71
sohagislampk <sohagislampk@gmail.com>
2022-11-24 14:33:10 +0600

    Register page created

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -27,11 +27,32 @@ const Register = () => {
                                 <span className="label-text">Password</span>
                             </label>
                             <input type="text" placeholder="password" className="input input-bordered" />
-                            <label className="label">
-                                <p >Already have Account ? <Link to={'/login'} className="text-primary link link-hover">Login</Link ></p>
-                            </label>
                         </div>
-                        <div className="form-control mt-6">
+                        <div className="form-control w-full max-w-lg">
+                            <label className="label">
+                                <span className="label-text">Profile picture</span>
+                            </label>
+                            <input type="file" className="file-input file-input-bordered w-full max-w-lg" />
+                        </div>
+                        <div className='flex justify-end items-center'>
+                            <p>Register As : </p>
+                            <div className="form-control">
+                                <label className="label cursor-pointer">
+                                    <span className="label-text mr-2">Seller</span>
+                                    <input type="radio" name="radio-10" className="radio checked:bg-primary" checked />
+                                </label>
+                            </div>
+                            <div className="form-control">
+                                <label className="label cursor-pointer">
+                                    <span className="label-text mr-2">Buyer</span>
+                                    <input type="radio" name="radio-10" className="radio checked:bg-primary" checked />
+                                </label>
+                            </div>
+                        </div>
+                        <label className="label">
+                            <p >Already have Account ? <Link to={'/login'} className="text-primary link link-hover">Login</Link ></p>
+                        </label>
+                        <div className="form-control">
                             <button className="btn btn-accent text-white">Register</button>
                         </div>
                     </div>
